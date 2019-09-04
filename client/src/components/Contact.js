7d507a73e571f9d726da25b8d5f517991fd5aabe
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-09-04 15:55:42 -0400

    background

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,49 +1,60 @@
-import React from 'react';
-
-function Contact() {
+import React from "react";
+import "../pages/Contact.css"
+import "./background.css"
+const ContactForm = () => {
   return (
-    <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-md-offset-3">
-            <div className="well well-sm">
-              <form className="form-horizontal" action="" method="post">
-                <fieldset>
-                  <legend className="text-center">Contact us</legend>
-
-                  <div className="form-group">
-                    <label className="col-md-3 control-label" for="name">Name</label>
-                    <div className="col-md-9">
-                      <input id="name" name="name" type="text" placeholder="Your name" className="form-control" />
-                    </div>
-                  </div>
-
-                  <div className="form-group">
-                    <label className="col-md-3 control-label" for="email">Your E-mail</label>
-                    <div className="col-md-9">
-                      <input id="email" name="email" type="text" placeholder="Your email" className="form-control" />
-                    </div>
-                  </div>
-
-                  <div className="form-group">
-                    <label className="col-md-3 control-label" for="message">Your message</label>
-                    <div className="col-md-9">
-                      <textarea className="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="col-md-12 text-right">
-                      <button type="submit" className="btn btn-primary btn-lg">Submit</button>
-                    </div>
-                  </div>
-                </fieldset>
-              </form>
+  <div className = 'container-fluid bg'>
+    
+    <div className="col-md-6 col-12 mx-auto">
+      <div className="contact-card card shadow mt-4 mb-5">
+        <div className="card-header text-center">Send Me a Message</div>
+        <div className="card-body">
+          <form
+            action="https://formspree.io/mrubenstein6192@gmail.com"
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="name">Your Name</label>
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Name"
+                name="name"
+              />
+              
             </div>
-          </div>
+            <label htmlFor="email">Your Email</label>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                placeholder="your_email@email.com"
+                name="_replyto"
+              />
+             
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleFormControlTextarea1">Message</label>
+              <textarea
+                placeholder="Write message here."
+                className="form-control"
+                id="text-area"
+                type="text"
+                rows="3"
+                name="message"
+              />
+            </div>
+            <div className="form-group">
+              <input className="btn btn-primary" type="submit" value="Send" />
+            </div>
+          </form>
         </div>
       </div>
-    </footer>
-  )
-}
+    </div>
 
-export default Contact;
+    
+  </div>
+  );
+};
+
+export default ContactForm;
